ae58b8461b78c57aadaca309d6a2ab00f21a7168
Drew-Linky <34217778+Drew-Linky@users.noreply.github.com>
2022-04-23 04:59:30 -0500

    Add files via upload

---
+++ lessons.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>test</p>
+    <?php
+        echo "hellu";
+        print "testy";
+        echo 10+5;
+        $name = "Daniel";
+        echo $name;
+    ?>
+</body>
+</html>
